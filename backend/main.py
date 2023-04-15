1dbae3ef202ce0ad296d8e078e33e783f69843b0
Olympicene <kevinjcord@gmail.com>
2023-04-15 16:42:10 -0500

    added new rand functions

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,33 @@
 import requests
 import os
+import random
 from fastapi import FastAPI
 from dotenv import load_dotenv
 
 load_dotenv()
-THEMOVEDB_API_KEY = os.getenv('THEMOVEDB_API_KEY')
+THEMOVIEDB_API_KEY = os.getenv('THEMOVIEDB_API_KEY')
+
+# UGLY CONTSTANTS
+MAX_MOVIE_ID = 1113623
+MAX_TVSHOW_ID = 224579
+
+def random_MovieID():
+	not_valid = True
+	while not_valid:
+		random_id = random.randint(0, MAX_MOVIE_ID)
+		response = requests.get(f"https://api.themoviedb.org/3/movie/{random_id}?api_key={THEMOVIEDB_API_KEY}")
+		if not hasattr(response.json(), 'status_code'):
+			not_valid = False
+	return (response.json()['id'])
+
+def random_TVShowID():
+	not_valid = True
+	while not_valid:
+		random_id = random.randint(0, MAX_TVSHOW_ID)
+		response = requests.get(f"https://api.themoviedb.org/3/tv/{random_id}?api_key={THEMOVIEDB_API_KEY}")
+		if not hasattr(response.json(), 'status_code'):
+			not_valid = False
+	return (response.json()['id'])
 
 
 app = FastAPI()
