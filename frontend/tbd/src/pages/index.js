65f455fa3929955b8cbc1330ae8f0e626c90b87d
dejazzhands <sameffendy08@gmail.com>
2023-04-15 15:07:01 -0500

    added next.js initial app.

---
+++ frontend/tbd/src/pages/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function App(){
+  return(
+    <Frame1 loremipsum1="lorem ipsum" loremipsum2="lorem ipsum"
+      buttonsProps={frame1Data.buttonsProps}
+      buttons2Props={frame1Data.buttons2Props}
+      buttons3Props={frame1Data.buttons3Props}
+    
+    />
+  );
+}
+
+
+export default App;
