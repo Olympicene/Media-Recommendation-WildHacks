724e042b4ac0e2ac5209cc99467fa49c67b9739b
Olympicene <kevinjcord@gmail.com>
2023-04-15 19:31:12 -0500

    finished /rand endpoint

--- backend/main.py
+++ backend/main.py
@@ -13,56 +13,44 @@ MAX_MOVIE_ID = 1113623
 MAX_TVSHOW_ID = 224579
 MAX_GAME_ID = 58134
 
+# return a ('movie', movieID)
 def random_MovieID():
 	not_valid = True
 	while not_valid:
 		random_id = random.randint(0, MAX_MOVIE_ID)
-		response = requests.get(f"https://api.themoviedb.org/3/movie/{random_id}?api_key={THEMOVIEDB_API_KEY}")
-		if not hasattr(response.json(), 'status_code'):
+		response = requests.get(f"https://api.themoviedb.org/3/movie/{random_id}?api_key={THEMOVIEDB_API_KEY}").json()
+		if 'id' in response:
 			not_valid = False
-	return ('movie', response.json()['id'])
+	return ('movie', response['id'])
 
+# return a ('tv_show', tvshowID)
 def random_TVShowID():
 	not_valid = True
 	while not_valid:
 		random_id = random.randint(0, MAX_TVSHOW_ID)
-		response = requests.get(f"https://api.themoviedb.org/3/tv/{random_id}?api_key={THEMOVIEDB_API_KEY}")
-		if not hasattr(response.json(), 'status_code'):
+		response = requests.get(f"https://api.themoviedb.org/3/tv/{random_id}?api_key={THEMOVIEDB_API_KEY}").json()
+		if 'id' in response:
 			not_valid = False
-	return ('tv', response.json()['id'])
+	return ('tv_show', response['id'])
 
+# return a ('videogame', videogameID)
 def random_GamesID():
 	not_valid = True
 	while not_valid:
 		random_id = random.randint(0, MAX_GAME_ID)
-		response = requests.get(f"https://api.rawg.io/api/games/{random_id}?key={RAWG_API_KEY}")
-		if not hasattr(response.json(), 'detail'):
+		response = requests.get(f"https://api.rawg.io/api/games/{random_id}?key={RAWG_API_KEY}").json()
+		if 'detail' not in response:
 			not_valid = False
-	return (response.json()['id'])
+	return ('videogame',  response['id'])
+
+# return a list of 2 multimedia tuples
+def random_multimedia_pair():
+	multimedia = [random_GamesID, random_MovieID, random_TVShowID]
+	random.choice(multimedia)()
+	return {random.choice(multimedia)(), random.choice(multimedia)()}
 
 app = FastAPI()
 
 @app.get("/rand")
-def read_root():
-
-    headers = {
-        "Authorization": f"Bearer {THEMOVIEDB_API_KEY}",
-        "Content-Type": "application/json;charset=utf-8",
-    }
-
-    response = requests.get('https://api.themoviedb.org/3/movie/76341', headers=headers)
-    print(response)
-
-    return {
-        "Type1": "x1",
-        "ID1": "y1",
-        "Typ2": "x2",
-        "ID2": "y2",
-        "score": 100,
-        "total": 1000,
-    }
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+def random_pair():
+    return random_multimedia_pair()
