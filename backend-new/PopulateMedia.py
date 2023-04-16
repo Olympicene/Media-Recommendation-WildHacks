7875bc3f585c440c5b71584b7837ad60051c4fdd
dejazzhands <sameffendy08@gmail.com>
2023-04-16 12:13:44 -0500

    added verification method to filter PopulateMedia.py

--- backend-new/PopulateMedia.py
+++ backend-new/PopulateMedia.py
@@ -13,6 +13,21 @@ dbconn = db.create_connection(DB_FILE)
 db.create_media_table(dbconn)
 db.create_pair_table(dbconn)
 
-db.add_media(dbconn, random.Movie())
-db.add_media(dbconn, random.TVShow())
-db.add_media(dbconn, random.Game())
+
+movie = random.Movie()
+tvshow = random.TVShow()
+game = random.Game()
+
+print("Movie: ", movie)
+print("TV Show: ", tvshow)
+print("Game: ", game)
+approve = input("Do you want to add this media to the media file? (y/n): ")
+
+if approve.lower() == "y":
+    db.add_media(dbconn, movie)
+    # db.add_media(dbconn, tvshow)
+    # db.add_media(dbconn, game)
+    print("Media added to media file.")
+else:
+    print("Media not added to media file.")
+
