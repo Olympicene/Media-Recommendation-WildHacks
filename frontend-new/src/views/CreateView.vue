120a9796412e80699e1b1c9c5e0e3f65d606f72d
sharatd <sharat.lakers24@gmail.com>
2023-04-16 11:58:52 -0500

    add html media request form

--- frontend-new/src/views/CreateView.vue
+++ frontend-new/src/views/CreateView.vue
@@ -1,8 +1,43 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <body>
+  <center>
+    <h1><tt> Media Requests </tt></h1>
+    <div class="boxed">
+      <h2><tt> Name </tt></h2>
+      <input type="text" id="Textbox" name="Name">
+      <h2><tt> Media Type </tt></h2>
+      <input type="text" id="Textbox" name="Type">
+      <h2><tt> Pictures of Media </tt></h2>
+      <input type="text" id="Textbox" name="Pics">
+    </div>
+  </center>
+  </body>
 </template>
 
 <style>
+  #Textbox {
+    border-radius: 10px;
+    border: 1px solid #73AD21;
+    width: 200px;
+    height: 50px;
+  }
+
+  .boxed {
+    border: 1px solid black;
+    padding: 10px;
+    width: 300px;
+    padding-bottom: 50px;
+  }
+
+  h1 {
+    text-align: center;
+  }
+
+  h2 {
+    text-align: center;
+  }
+
+  body {
+    background-color: #EADDCA;
+  }
 </style>
